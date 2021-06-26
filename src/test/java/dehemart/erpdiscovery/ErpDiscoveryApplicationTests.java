4134eebd6b15aceaf3ef7ae899ac9c402ad10a57
dehemart <martins.denis@gmail.com>
2021-06-26 11:58:25 -0300

    Inital from discovery

---
+++ src/test/java/dehemart/erpdiscovery/ErpDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dehemart.erpdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
